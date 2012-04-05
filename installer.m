08a3d150fbc27567a60706259dd454147bedad70
Alexandre Barachant <alexandre.barachant@gmail.com>
2012-04-05 11:45:38 +0200

    add installation script

---
+++ installer.m
@@ -0,0 +1,13 @@
+% Installer for the Covariance toolbox
+
+HOME = pwd;
+path([HOME,'\lib'],path);
+path([HOME,'\lib\distance'],path);
+path([HOME,'\lib\geodesic'],path);
+path([HOME,'\lib\riemann'],path);
+path([HOME,'\lib\visu'],path);
+path([HOME,'\lib\estimation'],path);
+path([HOME,'\lib\mean'],path);
+path([HOME,'\lib\simulation'],path);
+
+disp('Covariance toolbox activated');
