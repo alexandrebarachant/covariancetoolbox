3cb9aab8aaa8ef7970a8d24d13cf32d5b463bcbe
Alexandre Barachant <alexandre.barachant@gmail.com>
2012-04-05 11:02:12 +0200

    add geodesic

---
+++ geodesic/geodesic.m
@@ -0,0 +1,14 @@
+function C = geodesic(C1,C2,t,metric)
+
+if (nargin<4)||(isempty(metric))
+    metric = 'euclid';
+end
+
+switch metric
+    case 'riemann'
+        C = riemann_geodesic(C1,C2,t);
+    case 'logeuclid'
+        C = logeuclidean_geodesic(C1,C2,t); 
+    otherwise
+        C = euclidean_geodesic(C1,C2,t);
+end
