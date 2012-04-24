e9ddbdebe5b77ac6bc0f91aa6a14a81aed72955d
Alexandre Barachant <alexandre.barachant@gmail.com>
2012-04-24 15:17:37 +0200

    installer for unix

--- installer.m
+++ installer.m
@@ -1,13 +1,25 @@
 % Installer for the Covariance toolbox
 
-HOME = pwd;
-path([HOME,'\lib'],path);
-path([HOME,'\lib\distance'],path);
-path([HOME,'\lib\geodesic'],path);
-path([HOME,'\lib\riemann'],path);
-path([HOME,'\lib\visu'],path);
-path([HOME,'\lib\estimation'],path);
-path([HOME,'\lib\mean'],path);
-path([HOME,'\lib\simulation'],path);
 
+HOME = pwd;
+
+if isunix
+    path([HOME,'/lib'],path);
+    path([HOME,'/lib/distance'],path);
+    path([HOME,'/lib/geodesic'],path);
+    path([HOME,'/lib/riemann'],path);
+    path([HOME,'/lib/visu'],path);
+    path([HOME,'/lib/estimation'],path);
+    path([HOME,'/lib/mean'],path);
+    path([HOME,'/lib/simulation'],path);
+else
+    path([HOME,'\lib'],path);
+    path([HOME,'\lib\distance'],path);
+    path([HOME,'\lib\geodesic'],path);
+    path([HOME,'\lib\riemann'],path);
+    path([HOME,'\lib\visu'],path);
+    path([HOME,'\lib\estimation'],path);
+    path([HOME,'\lib\mean'],path);
+    path([HOME,'\lib\simulation'],path);
+end    
 disp('Covariance toolbox activated');
